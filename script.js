3b5a0b0c33705d002699ab98ab465c7cc67b2304
gabrielagabriela <gabrielaa.slv@gmail.com>
2024-06-04 13:36:00 -0300

    Exercicio 02

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ let somaNumeros = arrayNumeros.reduce((ant, atual) => {
 console.log(somaNumeros)
 
 // Exercicio 02 Semana 04
-let pares = arrayNumeros.filter((x) => {
+let numerosPares = arrayNumeros.filter((x) => {
   return x % 2 == 0
 })
 
-console.log(pares)
+console.log(numerosPares)
